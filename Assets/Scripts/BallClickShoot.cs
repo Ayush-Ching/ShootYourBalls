cc5d17df797d65861bba3fb3ce3ea2060cc771fa
arshdeeps2006 <arshdeepsingh21747@gmail.com>
2025-06-18 21:55:20 +0530

    commit 3

--- Assets/Scripts/BallClickShoot.cs
+++ Assets/Scripts/BallClickShoot.cs
@@ -4,22 +4,16 @@ using UnityEngine.EventSystems;
 public class BallClickShoot : MonoBehaviour, IPointerClickHandler
 {
     private Rigidbody rb;
-    //private bool hasBeenShot = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        //rb.isKinematic = true;
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        //if (!hasBeenShot)
-        //{
             rb.isKinematic = false;
-            rb.AddForce(Camera.main.transform.up * 200f); // Adjust force as needed
-            rb.AddForce(Camera.main.transform.forward * 200f); // Adjust force as needed
-            //hasBeenShot = true;
-        //}
+            rb.AddForce(Camera.main.transform.up * 500f); // Adjust force as needed
+            rb.AddForce(Camera.main.transform.forward * 500f); // Adjust force as needed
     }
 }
