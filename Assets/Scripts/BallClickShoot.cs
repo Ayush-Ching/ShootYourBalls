c4ac3f89cebd875ab7550c98b69fd6fff30994c3
arshdeeps2006 <arshdeepsingh21747@gmail.com>
2025-06-19 17:26:42 +0530

    Swipe to Shoot

--- Assets/Scripts/BallClickShoot.cs
+++ Assets/Scripts/BallClickShoot.cs
@@ -1,19 +1,64 @@
 using UnityEngine;
-using UnityEngine.EventSystems;
 
-public class BallClickShoot : MonoBehaviour, IPointerClickHandler
+public class BallSwipeShoot : MonoBehaviour
 {
     private Rigidbody rb;
+    private Vector2 swipeStartPos;
+    private Vector2 swipeEndPos;
+    private bool isSwiping = false;
+
+    [SerializeField] private float forceMultiplier = 0.05f; // You can tweak this in Inspector
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    public void OnPointerClick(PointerEventData eventData)
+    void Update()
     {
-            rb.isKinematic = false;
-            rb.AddForce(Camera.main.transform.up * 500f); // Adjust force as needed
-            rb.AddForce(Camera.main.transform.forward * 500f); // Adjust force as needed
+        if (Input.touchSupported && Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            HandleTouch(touch.phase, touch.position);
+        }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            swipeStartPos = Input.mousePosition;
+            isSwiping = true;
+        }
+        else if (Input.GetMouseButtonUp(0) && isSwiping)
+        {
+            swipeEndPos = Input.mousePosition;
+            ShootFromSwipe();
+            isSwiping = false;
+        }
+    }
+
+    void HandleTouch(TouchPhase phase, Vector2 position)
+    {
+        switch (phase)
+        {
+            case TouchPhase.Began:
+                swipeStartPos = position;
+                break;
+            case TouchPhase.Ended:
+                swipeEndPos = position;
+                ShootFromSwipe();
+                break;
+        }
+    }
+
+    void ShootFromSwipe()
+    {
+        Vector2 swipeVector = swipeEndPos - swipeStartPos;
+        float swipeLength = swipeVector.magnitude;
+
+        if (swipeLength < 15f) return; // Ignore short swipes
+
+        Vector3 direction = new Vector3(swipeVector.x, swipeVector.y, swipeLength).normalized;
+        Vector3 forceDir = Camera.main.transform.TransformDirection(direction);
+
+        rb.isKinematic = false;
+        rb.AddForce(forceDir * swipeLength * forceMultiplier, ForceMode.Impulse);
     }
 }
